8300eb48ab03d68b76a8994deb364d0464c659ad
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2023-06-20 03:11:28 +0300

    Update Ahikka_t.py
    V 1.3.4(stable) changes:
    
    - Text edits, bug fixes.

--- Ahikka_t.py
+++ Ahikka_t.py
@@ -2,7 +2,7 @@ from telethon.sync import TelegramClient
 from telethon import events
 import sys
 
-#v 1.3.3(stable)
+#v 1.3.4(stable)
 
 if len(sys.argv) < 5:
     print("Недостаточно аргументов. Используйте: python Ahikka.py api_id api_hash user_id language")
@@ -25,7 +25,7 @@ def get_message_text(language, key):
             'not_enough_args': 'Not enough arguments. Usage: /a "<text>" <int> or /a off',
             'invalid_argument': 'Invalid argument format for <int>. Usage: /a "<text>" <int> or /a off',
             'interval_zero': 'Interval value cannot be 0.',
-            'code_disabled': 'Code successfully disabled.',
+            'code_disabled': 'Code successfully disabled. To enable it again use /a "<text> "<int>',
             'code_already_disabled': 'Code is already disabled.',
             'success_message': 'Success. Wait for {} hour(s).',
             'off_message': 'Code already disabled.',
@@ -37,13 +37,13 @@ def get_message_text(language, key):
             'not_enough_args': 'Недостаточно аргументов. Используйте: /a "<text>" <int> или /a off',
             'invalid_argument': 'Неверный формат аргумента <int>. Используйте: /a "<text>" <int> или /a off',
             'interval_zero': 'Значение интервала не может быть 0.',
-            'code_disabled': 'Код успешно отключен.',
+            'code_disabled': 'Код успешно отключен. Чтобы его включить заново используйте /a "<text>" <int>',
             'code_already_disabled': 'Код уже отключен.',
             'success_message': 'Успешно. Ожидайте {} час(ов).',
             'off_message': 'Код уже отключен.',
-            'message_sent': 'Сообщение отправлено: {}',
-            'language_updated': 'Язык успешно изменен на русский.',
-            'language_already_set': 'Язык уже установлен на русский.'
+            'message_sent': 'Сообщение отправленно: {}',
+            'language_updated': 'Язык успешно изменен на Русский.',
+            'language_already_set': 'Язык уже установлен на Русский.'
         }
     }
     return messages[language][key]

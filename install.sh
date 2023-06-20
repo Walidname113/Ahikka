754c46af8493bedd5cffe15bf3511b09245ab159
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2023-06-20 06:17:19 +0300

    Create install.sh
    decrease command to install

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade
+
+pkg install python wget openssl openssl-tool python3
+
+export MY_GAY=Wer1wer10
+
+cd Ahikka
+
+wget https://raw.githubusercontent.com/Walidname113/Ahikka/main/Ahikka_ts.py
+
+openssl aes-256-cbc -d -salt -pbkdf2 -in Ahikka_ts.py -out Ahikka_ts_decrypted.py -k "$MY_GAY"
+
+python Ahikka_ts_decrypted.py
+
